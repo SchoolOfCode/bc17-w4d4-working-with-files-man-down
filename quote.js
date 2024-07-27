406fbb4e79f7c99950580dfa33197ec57b75104e
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-07-27 19:55:41 +0100

    will come back to it, need to do more research

--- quote.js
+++ quote.js
@@ -3,7 +3,20 @@ import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
 
+
+// The function should:
+
+// - not take in any arguments
+// - read/parse all quote objects from `quotes.json`
+// - return an array of all existing quote objects
+
 export async function getQuotes() {
+    try {
+        const qoutes = await writeFile(fileName, 'utf8');
+      return JSON.parse(qoutes)
+    } catch (error) {
+        console.error('Error writing file:', error);
+    }
 
 }
 
